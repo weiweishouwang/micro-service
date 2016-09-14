ccb08b4a7c1a31b19ccb959285d9b1df626b7c20
hongweizou <weiweishouwang@126.com>
2016-09-14 16:31:18 +0800

    init

--- demo-service/demo-service-api/src/main/java/com/zhw/ms/demo/api/DemoAPI.java
+++ demo-service/demo-service-api/src/main/java/com/zhw/ms/demo/api/DemoAPI.java
@@ -1,15 +1,9 @@
 package com.zhw.ms.demo.api;
 
 import com.zhw.ms.commons.bean.Result;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.awt.*;
 
 public interface DemoAPI {
 
--- demo-service/demo-service-server/src/main/java/com/zhw/ms/demo/service/DemoService.java
+++ demo-service/demo-service-server/src/main/java/com/zhw/ms/demo/service/DemoService.java
@@ -3,15 +3,8 @@ package com.zhw.ms.demo.service;
 import com.zhw.ms.demo.entity.Admin;
 import com.zhw.ms.demo.persistence.AdminMapper;
 import com.zhw.ms.middleware.db.SlaverDataSource;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Set;
 
 /**
  * Created by ZHW on 2015/5/8.
